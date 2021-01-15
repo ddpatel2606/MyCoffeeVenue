af86ea001bc270e9b415bea600af27456f03a562
dixitpatel <ddpatel2606@gmail.com>
2021-01-15 13:41:25 +0000

    - update unit test code.

--- app/src/test/java/com/dixitpatel/mycoffeevenue/MockUtil.kt
+++ app/src/test/java/com/dixitpatel/mycoffeevenue/MockUtil.kt
@@ -4,30 +4,45 @@ import com.dixitpatel.mycoffeevenue.model.*
 
 object MockUtil {
 
-  fun location() = Location("Unit 49 First Floor","Wembley Park Boulevard",51.55632269860604,
-  -0.28291010681428796,1073, "HA9 0FD",
-"GB",
-  "Tokyngton",
-  "Greater London",
-  "United Kingdom",
-  listOf(
-  "Unit 49 First Floor (Wembley Park Boulevard)", "Tokyngton", "Greater London", "HA9 0FD", "United Kingdom",
-  ),"k")
-
-  fun category() = Category("4bf58dd8d48988d1e0931735",
-  "Coffee Shop", Icon("https://ss3.4sqi.net/img/categories_v2/food/coffeeshop_",
-    ".png")
+  fun location() = NearByPlaceResponse.Location(
+    "Unit 49 First Floor", "Wembley Park Boulevard", 51.55632269860604,
+    -0.28291010681428796, 1073, "HA9 0FD",
+    "GB",
+    "Tokyngton",
+    "Greater London",
+    "United Kingdom",
+    listOf(
+      "Unit 49 First Floor (Wembley Park Boulevard)",
+      "Tokyngton",
+      "Greater London",
+      "HA9 0FD",
+      "United Kingdom",
+    ), "k"
   )
 
-  fun venue() = Venue("528490bf11d24bf0d288fb81",name = "Costa Coffee", location(), listOf(category()))
+  fun category() = NearByPlaceResponse.Category(
+    "4bf58dd8d48988d1e0931735",
+    "Coffee Shop", NearByPlaceResponse.Icon(
+      "https://ss3.4sqi.net/img/categories_v2/food/coffeeshop_",
+      ".png"
+    )
+  )
 
-  fun listOfItem() = Item(venue())
+  fun venue() = NearByPlaceResponse.Venue(
+    "528490bf11d24bf0d288fb81",
+    name = "Costa Coffee",
+    location(),
+    listOf(category())
+  )
 
-  fun listOfGroup() = Group("Recommended Places","Recommended", listOf(listOfItem()))
+  fun listOfItem() = NearByPlaceResponse.Item(venue())
+
+  fun listOfGroup() =
+    NearByPlaceResponse.Group("Recommended Places", "Recommended", listOf(listOfItem()))
 
   fun mockPlaces() = NearByPlaceResponse(
-    meta = Meta(200,"600163e609586a1e9ccf4cf4","",""),
-    response = Response("London",1, listOf(listOfGroup()))
+    meta = NearByPlaceResponse.Meta(200, "600163e609586a1e9ccf4cf4", "", ""),
+    response = NearByPlaceResponse.Response("London", 1, listOf(listOfGroup()))
   )
 
   fun mockNearByPlaceList() = listOf(mockPlaces())
